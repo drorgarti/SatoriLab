6e57bad2c01d6ee8baa97f915abc001bed974785
drorg <dror@acurerate.com>
2018-01-17 11:54:36 +0200

    1) Clearbit enhancements 2) Error handling

--- engagement/clearbit_engager.py
+++ engagement/clearbit_engager.py
@@ -49,39 +49,55 @@ class ClearbitEngager(Engager):
     def enrich_person(self):
 
         result_obj = self._get_person_info()
-        if result_obj['person'] is None:
-            return None
+
+        if 'pending' in result_obj and result_obj['pending']:
+            msg = 'Failed to get information on person %s. Pending (202)' % self.enrich_key
+            raise EngagementException(msg)
+
+        if 'person' not in result_obj or result_obj['person'] is None:
+            msg = 'Failed to get information on person %s. Not Found (404)' % self.enrich_key
+            raise EngagementException(msg)
+
+        enriched = False
+
         person_data = result_obj['person']
 
         # Get the name properties
         if 'name' in person_data:
-            self.set_data(P.FIRST_NAME, result_obj['person']['name']['givenName'])
-            self.set_data(P.LAST_NAME, result_obj['person']['name']['familyName'])
-            self.set_data(P.FULL_NAME, result_obj['person']['name']['fullName'])
+            self.set_data(P.FIRST_NAME, person_data['name']['givenName'])
+            self.set_data(P.LAST_NAME, person_data['name']['familyName'])
+            self.set_data(P.FULL_NAME, person_data['name']['fullName'])
 
-        if 'email' in person_data:
-            self.set_data(P.EMAIL, result_obj['person']['email'])
-            self.add_data(P.EMAILS, result_obj['person']['email'])
+        if 'email' in person_data and person_data['email'] != self.enrich_key:
+            self.set_data(P.EMAIL, person_data['email'])
+            self.add_data(P.EMAILS, person_data['email'])
 
         if 'gender' in person_data and person_data['gender']:
+            #enriched = True
             self.add_data(P.GENDER, person_data['gender'])
 
         if 'bio' in person_data and person_data['bio']:
+            enriched = True
             self.add_data(P.SHORT_DESCRIPTION, person_data['bio'])
 
         if 'location' in person_data and person_data['location']:
+            enriched = True
             self.add_data(P.LOCATIONS, person_data['location'])
 
         if 'facebook' in person_data and person_data['facebook']['handle']:
+            enriched = True
             self.add_data(P.FACEBOOK_URL, person_data['facebook']['handle'])
 
         if 'linkedin' in person_data and person_data['linkedin']['handle']:
+            enriched = True
             self.add_data(P.LINKEDIN_URL, result_obj['person']['linkedin'])
 
         if 'twitter' in person_data and person_data['twitter']['handle']:
+            enriched = True
             self.add_data(P.TWITTER_URL, result_obj['person']['twitter'])
 
         if 'googleplus' in person_data and person_data['googleplus']['handle']:
+            enriched = True
             self.add_data(P.GOOGLEPLUS_URL, result_obj['person']['googleplus'])
 
         if 'employment' in person_data:
@@ -93,14 +109,32 @@ class ClearbitEngager(Engager):
             if person_data['employment'].get('role', None) is not None:
                 job[P.JOB_ROLE] = person_data['employment'].get('role', [])
             if job != {}:
+                enriched = True
                 self.add_data(P.JOBS, job)
 
         # TODO: gravatar, aboutme, github
 
+        if not enriched:
+            msg = 'Failed: no information added to person %s' % self.enrich_key
+            raise EngagementException(msg)
 
         return [P.JOBS]
 
     def enrich_company(self):
+
+        result_obj = self._get_company_info()
+
+        if 'pending' in result_obj and result_obj['pending']:
+            msg = 'Failed to get information on person %s. Pending (202)' % self.enrich_key
+            raise EngagementException(msg)
+
+        if 'company' not in result_obj or result_obj['company'] is None:
+            msg = 'Failed to get information on company %s. Not Found (404)' % self.enrich_key
+            raise EngagementException(msg)
+
+        enriched = False
+
+        company_data = result_obj['company']
 
         return [C.NAME]
 
@@ -108,6 +142,17 @@ class ClearbitEngager(Engager):
 
         try:
             response = clearbit.Enrichment.find(email=self.enrich_key)
+        except EngagementException as e:
+            raise e
+        except Exception as e:
+            raise EngagementException(e, True)
+
+        return response
+
+    def _get_company_info(self):
+
+        try:
+            response = clearbit.Company.find(domain=self.enrich_key, stream=True)
         except EngagementException as e:
             raise e
         except Exception as e:
